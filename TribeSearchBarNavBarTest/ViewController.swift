76a7618921fc3fbc9df68ac1055f309e3f3df4d1
Richmond Ko <richmond@tribegroup.co>
2019-04-05 10:27:20 +0800

    Initial Commit

---
+++ TribeSearchBarNavBarTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TribeSearchBarNavBarTest
+//
+//  Created by Richmond Ko on 05/04/2019.
+//  Copyright © 2019 Richmond Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
